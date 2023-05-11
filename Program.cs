1f8b63d8ce3031ed52c5bf5b3c3a406ded0200ef
Memlak <potowik67@gmail.com>
2023-05-11 14:22:09 +0200

    kometarz

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierwsza_konsola
+{
+    internal class Uczen
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.Title = "Moja Konsola!";
+            Console.WriteLine("Klasa 2P");
+            Console.WriteLine("Grupa 1");
+            Console.WriteLine("Mechaniak");
+            Console.Clear();
+            Console.ResetColor();
+            Console.WriteLine("Caps lock: " + Console.CapsLock); 
+            Console.ReadKey();
+
+        }
+    }
+}
